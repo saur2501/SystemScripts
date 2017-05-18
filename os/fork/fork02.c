3f1a7cce7584c850a45de3364a3cd62787d876e8
Saurabh Devgun <Saurabh Devgun>
2017-05-18 23:28:35 +0530

    os using syscalls added

---
+++ os/fork/fork02.c
@@ -0,0 +1,29 @@
+/* filename: fork-02.c */
+#include <stdio.h>
+#include <sys/types.h>
+#define MAX_COUNT 200
+void ChildProcess(void); /* child process prototype */
+void ParentProcess(void); /* parent process prototype */
+void main(void)
+{
+pid_t pid;
+pid = fork();
+if (pid == 0)
+ChildProcess();
+else
+ParentProcess();
+}
+void ChildProcess(void)
+{
+int i;
+for (i = 1; i <= MAX_COUNT; i++)
+printf("This line is from child, value = %d\n", i);
+printf("### Child process is done ###\n");
+}
+void ParentProcess(void)
+{
+int i;
+for (i = 1; i <= MAX_COUNT; i++)
+printf("This line is from parent, value = %d\n", i);
+printf("*** Parent process is done ***\n");
+}
